a26c425c89cc9702209e159d67db46d33e1cedb0
guru3k1 <cesargamadori@gmail.com>
2019-05-26 19:15:52 -0300

    Planet Class

---
+++ src/main/java/com/cga/weatherReport/model/Planet.java
@@ -0,0 +1,46 @@
+package com.cga.weatherReport.model;
+
+public class Planet {
+	private int radius;
+	private int angularSpeed;
+	private int direction;
+	
+	public Coordinates getCoordinates(int day) {
+		return new Coordinates(getX(day),getY(day));
+	}
+	
+	public double getY (int day) {
+		return this.radius * Math.sin(Math.toRadians(this.angularSpeed) * day* this.direction);
+	}
+	
+	public double getX (int day) {
+		return this.radius * Math.cos(Math.toRadians(this.angularSpeed) * day* this.direction);
+	}
+	
+	public Planet(int radius, int angularSpeed, int direction) {
+		super();
+		this.radius = radius;
+		this.angularSpeed = angularSpeed;
+		this.direction = direction;
+	}
+	public int getRadius() {
+		return radius;
+	}
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	public int getAngularSpeed() {
+		return angularSpeed;
+	}
+	public void setAngularSpeed(int angularSpeed) {
+		this.angularSpeed = angularSpeed;
+	}
+	public int getDirection() {
+		return direction;
+	}
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
+	
+	
+}
